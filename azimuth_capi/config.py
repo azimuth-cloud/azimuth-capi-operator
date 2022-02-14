b07305dc63ff46e44e45f1d7f82caf0ef502a2bb
Matt Pryor <matt@stackhpc.com>
2022-02-14 10:12:32 +0000

    Bump default Zenith chart version

--- azimuth_capi/config.py
+++ azimuth_capi/config.py
@@ -70,7 +70,7 @@ class ZenithConfig(Section):
     #: The version of the charts to use
     #: When changing this, be aware that the operator may depend on the layout of
     #: the Helm values at a particular version
-    chart_version: SemVerVersion = "0.1.0-dev.0.main.146"
+    chart_version: SemVerVersion = "0.1.0-dev.0.main.147"
 
     #: Defaults for use with the apiserver chart
     apiserver_defaults: t.Dict[str, t.Any] = Field(default_factory = dict)
