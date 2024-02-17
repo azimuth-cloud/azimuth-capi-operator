dbf264537b25a6e9dd9452fee0a23d9c2ee779f7
Matt Pryor <matt@stackhpc.com>
2024-02-17 15:12:35 +0000

    Apply kopf tunings to reduce operator stalls (#127)
    * Apply kopf tuning to reduce operator stalls
    
    * Give Kubernetes a chance to create the APIs

--- azimuth_capi/operator.py
+++ azimuth_capi/operator.py
@@ -1,3 +1,4 @@
+import asyncio
 import base64
 import datetime as dt
 import functools
@@ -78,14 +79,32 @@ async def apply_settings(**kwargs):
         certfile = settings.webhook.certfile,
         pkeyfile = settings.webhook.keyfile
     )
+    kopf_settings.watching.client_timeout = settings.watch_timeout
     if settings.webhook.managed:
         kopf_settings.admission.managed = f"webhook.{settings.api_group}"
-    try:
-        for crd in registry:
+    # Apply the CRDs
+    for crd in registry:
+        try:
             await ekclient.apply_object(crd.kubernetes_resource(), force = True)
-    except Exception:
-        logger.exception("error applying CRDs - exiting")
-        sys.exit(1)
+        except Exception:
+            logger.exception("error applying CRD %s.%s - exiting", crd.plural_name, crd.api_group)
+            sys.exit(1)
+    # Give Kubernetes a chance to create the APIs for the CRDs
+    await asyncio.sleep(0.5)
+    # Check to see if the APIs for the CRDs are up
+    # If they are not, the kopf watches will not start properly so we exit and get restarted
+    for crd in registry:
+        preferred_version = next(k for k, v in crd.versions.items() if v.storage)
+        api_version = f"{crd.api_group}/{preferred_version}"
+        try:
+            _ = await ekclient.get(f"/apis/{api_version}/{crd.plural_name}")
+        except Exception:
+            logger.exception(
+                "api for %s.%s not available - exiting",
+                crd.plural_name,
+                crd.api_group
+            )
+            sys.exit(1)
 
 
 @kopf.on.cleanup()
