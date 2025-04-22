37421361fba52f4a09b090834280a6664c362c47
John Garbutt <john.garbutt@stackhpc.com>
2025-04-22 16:56:32 +0100

    Add new test for the new code

--- azimuth_capi/tests/test_operator.py
+++ azimuth_capi/tests/test_operator.py
@@ -65,6 +65,39 @@ class TestOperator(unittest.IsolatedAsyncioTestCase):
         operator.settings.capi_helm.default_values = {
             'extra_things': {'enabled': True},
         }
+
+        result = operator.generate_helm_values_for_release(template, cluster)
+
+        self.assertDictEqual(result, {
+            'addons': {'ingress': {'enabled': False},
+            'kubernetesDashboard': {'enabled': False},
+            'monitoring': {'enabled': False}},
+            'cloudCredentialsSecretName': 'secret1',
+            'controlPlane': {'healthCheck': {'enabled': True},
+                            'machineFlavor': 'vm.small'},
+            'extra_things': {'enabled': True},
+            'kubernetesVersion': 'v1.31.0',
+            'machineImageId': '12456789',
+            'nodeGroupDefaults': {'healthCheck': {'enabled': True}},
+            'nodeGroups': [
+                {'autoscale': False,
+                 'machineCount': 2,
+                 'machineFlavor': 'vm.small',
+                 'name': 'vms'},
+                {'autoscale': False,
+                 'machineCount': 2,
+                 'machineFlavor': 'bm.gpus',
+                 'name': 'gpus'},
+            ],
+        })
+
+    def test_generate_helm_values_for_release_with_overrides(self):
+        cluster = self.get_fake_cluster()
+        template = self.get_fake_cluster_template()
+
+        operator.settings.capi_helm.default_values = {
+            'extra_things': {'enabled': True},
+        }
         operator.settings.capi_helm.flavor_specific_node_group_overrides = {
             'bm.*': {
                 'kubeadmConfigSpec': {
